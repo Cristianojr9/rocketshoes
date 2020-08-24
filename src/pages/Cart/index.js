8dc247e74b64bc7a03c8724930c40dd77f1ac2d8
Cristiano <cristianojr@alu.ufc.br>
2020-08-24 16:18:31 -0300

    :construction: Home page in progress

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,46 @@
-import React from 'react'
+import React from 'react';
+
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
   return (
-    <div />
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img 
+                src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/26/HZM-1731-026/HZM-1731-026_zoom2.jpg?ts=1571078789&ims=326x" 
+                alt=""
+              />
+            </td>
+            <td>
+              <strong>Tenis maneiro</strong>
+              <span>R$ 139,00</span>
+            </td>
+            <td>
+              
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar pedido</button>
+
+        <Total>
+          <span>Total</span>
+          <strong>R$ 199,00</strong>
+        </Total>
+      </footer>
+    </Container>
   )
 }
